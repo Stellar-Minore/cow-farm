7e5aa53e466c6d8ff610d508cbdc4272e84efa35
Muhammad Sharjeel Maqsood <m.sharjeel193@gmail.com>
2023-07-11 07:34:55 +0500

    Add cow section pages to app routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Styles from './app.module.scss';
 import { LandingScreen } from '../landingScreen';
 import { CowFarmScreen } from '../cowFarmScreen';
+import { MilkScreen } from '../milkScreen';
+import { HayDeliveryScreen } from '../hayDeliveryScreen';
+import { ThanksScreen } from '../thanksScreen';
 
 function App() {
 	return (
@@ -10,6 +13,9 @@ function App() {
 				<Routes>
 					<Route path="/" element={<LandingScreen />} />
 					<Route path="/cow-farm" element={<CowFarmScreen />} />
+					<Route path="/cow-farm/milk" element={<MilkScreen />} />
+					<Route path="/cow-farm/hay-delivery" element={<HayDeliveryScreen />} />
+					<Route path="/cow-farm/thanks" element={<ThanksScreen />} />
 				</Routes>
 			</Layout>
 		</div>
